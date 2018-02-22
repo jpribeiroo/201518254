4631d0436434882c808c19cd587a4d5a9ce81bb7
201518254 <201518254@MOCLI10231.animaedu.intranet>
2018-02-21 22:21:52 -0300

    Primeira versao

---
+++ 201518254/src/pr/usjt/ftce/arqsw/entility/fila.java
@@ -0,0 +1,21 @@
+package pr.usjt.ftce.arqsw.entility;
+
+public class fila {
+
+private int id;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+private String nome;
+
+
+}
